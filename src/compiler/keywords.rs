ef4b9ca91ee803848b7ebcd772e76bd48563d304
fade <truefadeoffical@gmail.com>
2024-01-12 19:13:15 +0100

    Add return keyword

--- src/compiler/keywords.rs
+++ src/compiler/keywords.rs
@@ -1,11 +1,13 @@
 #[derive(Debug)]
 pub(crate) enum Keyword {
     Use,
+    Return,
 }
 
 pub(crate) fn get_keyword(string: &str) -> Option<Keyword> {
     match string {
         "use" => Some(Keyword::Use),
+        "return" => Some(Keyword::Return),
         _ => None,
     }
 }
